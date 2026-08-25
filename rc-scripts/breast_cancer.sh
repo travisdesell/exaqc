7e35afdc95df4dcb1ed93a11e4adc9844f1ad5dd
Devroop Kar <dk7405@rit.edu>
2026-08-25 10:01:48 -0400

    added new breast_cancer script

--- rc-scripts/breast_cancer.sh
+++ rc-scripts/breast_cancer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#SBATCH -J breast_cancer
-#SBATCH -t 1-00:00:00
-#SBATCH -o ./outs/breast_cancer/output.o
-#SBATCH -e ./logs/breast_cancer/error.e
+#SBATCH -J breast_cancer_linear_u3
+#SBATCH -t 3-00:00:00
+#SBATCH -o ./outs/breast_cancer/output_lu3.o
+#SBATCH -e ./logs/breast_cancer/error_lu3.e
 #SBATCH -A cps -p tier3
 #SBATCH --nodes=1
 #SBATCH --ntasks=12
@@ -16,29 +16,34 @@ source .venv/bin/activate
 
 DATASET="breast_cancer"
 QUBITS=5
-ENCODING="identity"
-QUANTUM_ENC="amplitude"
+ENCODING="linear"
+QUANTUM_ENC="u3"
 BATCH_SIZE=8
 N_GENOMES=2000
 
-srun python3 -m src.examples.classification \
-    --dataset $DATASET \
-    --target pennylane \
-    --encoding $ENCODING \
-    --decoding linear \
-    --input_qubits $QUBITS \
-    --output_qubits $QUBITS \
-    --quantum_input_mode $QUANTUM_ENC \
-    --quantum_output_mode probs \
-    --batch_size $BATCH_SIZE \
-    --validation_batch_size $BATCH_SIZE \
-    --epochs 100 \
-    --learning_rate 0.001 \
-    --weight_decay 0.0005 \
-    --number_genomes $N_GENOMES \
-    --mutation_strategy uniform 1 5 \
-    --parent_strategy uniform 2 5 \
-    --seed 42 \
-    --out_dir artifacts/${DATASET}_${ENCODING}_${QUANTUM_ENC}_g${N_GENOMES}_q${QUBITS}_b${BATCH_SIZE} \
-    steady_state \
-    --max_population_size 30
+MIN_COUNT=$1
+MAX_COUNT=$2
+
+for i in $(seq $MIN_COUNT $MAX_COUNT); do
+    srun python3 -m src.examples.classification \
+        --dataset $DATASET \
+        --target pennylane \
+        --encoding $ENCODING \
+        --decoding linear \
+        --input_qubits $QUBITS \
+        --output_qubits $QUBITS \
+        --quantum_input_mode $QUANTUM_ENC \
+        --quantum_output_mode probs \
+        --batch_size $BATCH_SIZE \
+        --validation_batch_size $BATCH_SIZE \
+        --epochs 100 \
+        --learning_rate 0.001 \
+        --weight_decay 0.0005 \
+        --number_genomes $N_GENOMES \
+        --mutation_strategy uniform 1 5 \
+        --parent_strategy uniform 2 5 \
+        --seed 42 \
+        --out_dir artifacts/${DATASET}_${ENCODING}_${QUANTUM_ENC}_g${N_GENOMES}_q${QUBITS}_b${BATCH_SIZE}/runs/${i} \
+        steady_state \
+        --max_population_size 30
+done
