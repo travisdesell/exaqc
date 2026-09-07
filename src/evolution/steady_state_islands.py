2b5b2b49a97ed5683fa4979408e8fc79cee6da74
Onkar Shelar <os9660@rit.edu>
2026-09-07 15:28:02 -0400

    sorts the snapshot of global population

--- src/evolution/steady_state_islands.py
+++ src/evolution/steady_state_islands.py
@@ -620,9 +620,11 @@ class SteadyStateIslands(PopulationStrategy):
             target_island = self.islands[genome.metadata["target_island_id"]]
 
         if self.profiler is not None:
-            merged_population = []
+            # sort the global population snapshot 
+            merged_population: list[CircuitGenome] = []
             for island in self.islands:
                 merged_population.extend(island.population)
+            merged_population.sort(key=cmp_to_key(self.compare))
 
             self.profiler.record(
                 step=self.insertions,
